fdd02abdf93ad90706e729ca06f491b7f5319986
Darko Maljkovic <darko2marko@gmail.com>
2026-02-03 00:54:12 +0100

    backend: add color field to Line model

--- backend/core/models.py
+++ backend/core/models.py
@@ -13,6 +13,7 @@ class Station(models.Model):
 class Line(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    color = models.CharField(max_length=20, blank = True)
 
     def __str__(self):
         return self.name
